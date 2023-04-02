34b1332b39ddc88ad8b398189e43d9a65fc6a85e
Ejim Favour <favourejim56@gmail.com>
2023-04-02 23:48:36 +0100

    modified the userslisttable component

--- src/components/UsersListTable/UsersListTable.tsx
+++ src/components/UsersListTable/UsersListTable.tsx
@@ -3,10 +3,9 @@ import classes from "./UsersListTable.module.scss";
 import { useAppSelector, useAppDispatch } from "../../hooks/reduxHooks";
 import { useState } from "react";
 import UserTableItem from "../UserTableItem/UserTableItem";
-import chevronLeft from "../../assets/dashboard/left-chevron.svg";
-import chevronRight from "../../assets/dashboard/right-chevron.svg";
 import FilterForm from "../FilterForm/FilterForm";
 import { usersListAction } from "../../slices/usersListSlice";
+import TablePagination from "../TablePagination/TablePagination";
 
 const tableHeaders = [
   "organization",
@@ -44,36 +43,9 @@ const UsersListTable = () => {
 
   const amountOfUsersPerPage = 10;
 
-  const pageNavigators = Array.from(
-    { length: amountOfUsersPerPage },
-    (_, i) => i + 1
-  );
-
   let startIndex = (currentPage - 1) * amountOfUsersPerPage;
 
   let endIndex = currentPage * amountOfUsersPerPage;
-
-  const decreaseCurrentPage = () => {
-    setCurrentPage((prevState: number) => {
-      if (prevState === 1) {
-        return prevState;
-      } else {
-        localStorage.setItem("currentPage", JSON.stringify(prevState - 1));
-        return prevState - 1;
-      }
-    });
-  };
-
-  const increaseCurrentPage = () => {
-    setCurrentPage((prevState: number) => {
-      if (prevState === 10) {
-        return prevState;
-      } else {
-        localStorage.setItem("currentPage", JSON.stringify(prevState + 1));
-        return prevState + 1;
-      }
-    });
-  };
 
   const usersTableList = useAppSelector((state) => state.usersList.tableList);
 
@@ -107,64 +79,10 @@ const UsersListTable = () => {
         </table>
         <FilterForm />
       </div>
-      <div className={classes["paginate"]}>
-        <div className={classes["paginate-page"]}>
-          <p>Showing</p>
-          <p className={classes["page-number"]}>{currentPage}</p>
-          <p>out of {amountOfUsersPerPage}</p>
-        </div>
-        <div className={classes["page-navigation"]}>
-          <div onClick={decreaseCurrentPage} className={classes.prev}>
-            <img src={chevronLeft} alt="chevron left" />
-          </div>
-
-          {currentPage + 2 < amountOfUsersPerPage - 2 ? null : <p>...</p>}
-          {pageNavigators
-            .slice(
-              currentPage - 1,
-              currentPage + 2 >= amountOfUsersPerPage - 2
-                ? amountOfUsersPerPage - 2
-                : currentPage + 2
-            )
-            .map((navigator: number, i: number) => (
-              <p
-                key={i}
-                className={`${classes.navigator} ${
-                  currentPage === navigator
-                    ? classes["active-navigator"]
-                    : classes["inactive-navigator"]
-                }`}
-                onClick={() => {
-                  setCurrentPage(navigator);
-                }}
-              >
-                {navigator}
-              </p>
-            ))}
-          {currentPage + 2 >= amountOfUsersPerPage - 2 ? null : <p>...</p>}
-          {pageNavigators
-            .slice(amountOfUsersPerPage - 2, amountOfUsersPerPage)
-            .map((navigator: number, i: number) => (
-              <p
-                key={i}
-                className={`${classes.navigator} ${
-                  currentPage === navigator
-                    ? classes["active-navigator"]
-                    : classes["inactive-navigator"]
-                }`}
-                onClick={() => {
-                  setCurrentPage(navigator);
-                }}
-              >
-                {navigator}
-              </p>
-            ))}
-
-          <div onClick={increaseCurrentPage} className={classes.next}>
-            <img src={chevronRight} alt="chevron right" />
-          </div>
-        </div>
-      </div>
+      <TablePagination
+        setCurrentPage={setCurrentPage}
+        currentPage={currentPage}
+      />
     </div>
   );
 };
